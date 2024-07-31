4b2a5195f567ced22f08d69e26f9b3e225f94701
Maria Burtseva <m4riyaburceva9898@yandex.ru>
2024-07-31 21:07:34 +0500

    add classes Park and Person

--- src/main/java/lesson_8/Park.java
+++ src/main/java/lesson_8/Park.java
@@ -1,2 +1,37 @@
-package lesson_8;public class Park {
+package lesson_8;
+
+public class Park {
+    private String name;
+
+    public Park(String name) {
+        this.name = name;
+    }
+
+    public class Attraktsion {
+        private String name;
+        private String workTime;
+        private int cost;
+
+        public Attraktsion(String name, String workTime, int cost) {
+            this.name = name;
+            this.workTime = workTime;
+            this.cost = cost;
+        }
+
+        public void printInfo() {
+            System.out.println("Название аттракциона: " + name);
+            System.out.println("Время работы: " + workTime);
+            System.out.println("Стоимость: " + cost);
+        }
+    }
+
+    public static void main(String[] args) {
+        Park park = new Park("Парк Горького");
+        Park.Attraktsion attr1 = park.new Attraktsion("Колесо обозрения", "10:00 - 22:00", 200);
+        Park.Attraktsion attr2 = park.new Attraktsion("Американские горки", "11:00 - 20:00", 300);
+
+        attr1.printInfo();
+        System.out.println();
+        attr2.printInfo();
+    }
 }
--- src/main/java/lesson_8/Person.java
+++ src/main/java/lesson_8/Person.java
@@ -1,2 +1,43 @@
-package lesson_8;public class Person {
+package lesson_8;
+
+public class Person {
+
+    private final String fullName;
+    private final String position;
+    private final String email;
+    private final String phone;
+    private final int salary;
+    private final int age;
+
+    public Person(String fullName, String position, String email, String phone, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void info() {
+        System.out.println("ФИО: " + fullName);
+        System.out.println("Должность: " + position);
+        System.out.println("Почта: " + email);
+        System.out.println("Телефон: " + phone);
+        System.out.println("Зарплата: " + salary);
+        System.out.println("Возраст: " + age);
+    }
+
+    public static void main(String[] args) {
+        Person[] personArray = new Person[5];
+        personArray[0] = new Person("Ivanov Ivan", "Engineer", "test0@mail.ru", "89000000000", 10000, 18);
+        personArray[1] = new Person("Ivanov Alex", "Engineer", "test1@mail.ru", "89111111111", 20000, 19);
+        personArray[2] = new Person("Ivanov Liam", "Engineer", "test2@mail.ru", "89222222222", 30000, 20);
+        personArray[3] = new Person("Ivanov Jacob", "Engineer", "test3@mail.ru", "89333333333", 40000, 30);
+        personArray[4] = new Person("Ivanov Ethan", "Engineer", "test4@mail.ru", "89444444444", 50000, 40);
+
+        for (Person person : personArray) {
+            person.info();
+            System.out.println();
+        }
+    }
 }
