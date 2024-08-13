0398dd904462e858af91f100f8c3bb1690aba7e0
Maria Burtseva <m4riyaburceva9898@yandex.ru>
2024-08-13 15:08:18 +0500

    changed classes Park and Person. Add classes MainPark and MainPerson

--- src/main/java/lesson_8/Park.java
+++ src/main/java/lesson_8/Park.java
@@ -1,16 +1,11 @@
 package lesson_8;
 
 public class Park {
-    private String name;
-
-    public Park(String name) {
-        this.name = name;
-    }
 
     public class Attraktsion {
-        private String name;
-        private String workTime;
-        private int cost;
+        private final String name;
+        private final String workTime;
+        private final int cost;
 
         public Attraktsion(String name, String workTime, int cost) {
             this.name = name;
@@ -23,15 +18,5 @@ public class Park {
             System.out.println("Время работы: " + workTime);
             System.out.println("Стоимость: " + cost);
         }
-    }
-
-    public static void main(String[] args) {
-        Park park = new Park("Парк Горького");
-        Park.Attraktsion attr1 = park.new Attraktsion("Колесо обозрения", "10:00 - 22:00", 200);
-        Park.Attraktsion attr2 = park.new Attraktsion("Американские горки", "11:00 - 20:00", 300);
-
-        attr1.printInfo();
-        System.out.println();
-        attr2.printInfo();
     }
 }
