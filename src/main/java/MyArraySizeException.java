2503d63fadc61d467b14a8e9f4b533b343080d78
Maria Burtseva <m4riyaburceva9898@yandex.ru>
2024-08-27 08:26:54 +0500

    Add homework Lesson_13

--- src/main/java/MyArraySizeException.java
+++
@@ -1,7 +0,0 @@
-//обработка исключений связанных с размером массива
-public class MyArraySizeException extends Exception {
-
-    public MyArraySizeException(String message) {
-        System.out.println("Подача массива другого размера.");
-    }
-}
